bf2e0cab29cf4ac5529cf3384db52328d6b246ac
farhangani619 <farhangani619@gmail.com>
2021-03-19 22:27:06 +0530

    added dotenv package for mongoose error

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './config/dev.env') })
 const authRoutes = require('./routes/authRoutes')
 const cookieParser = require('cookie-parser')
 const { requireAuth, checkUser } = require('./middleware/authMiddleware')
